04bff28a8c1fb7386f21af9d4757617ec1870c78
Ezequiel López <EzeMaxLop@hotmail.com>
2023-09-27 20:40:21 -0300

    Casi terminamos

--- Curso_Alejandro/Ejercicio2.py
+++ Curso_Alejandro/Ejercicio2.py
@@ -1,21 +1,36 @@
 stock_productos= {"Manzana": 10, "Leche": 5, "Pan": 3, "Factura": 0}
 precio_productos= {"Manzana": 3.5, "Leche": 5.5, "Pan": 3.5, "Factura": 2.75}
 
-def pedidos_procesados(pedidos):
+def procesar_pedidos(pedidos):
+    precio_total= 0
+    estado= None
+    pedidos_procesados= []
 
-    for a in pedidos:
-        #print(a)
-        for value in a.values():
+    for pedido in pedidos:
+        #print(pedido)
+        for value in pedido.values():
             if type(value) == dict: 
+                #print(value.keys())
                 for key, valor in value.items():
-                    print(type(valor))
-                    #if valor < stock_productos.get(key):
-                    #    print("SIIII")
-                    #    stock_productos.update({key: stock_productos.get(key) - valor})
-                    #    print(stock_productos)
-                    #else:
-                    #    print("BASTA")
+                #    print(type(valor))
+                #    print(key)
+                #    print(stock_productos.get(key))
+                    if valor < stock_productos.get(key):
+                        #print("SIIII")
+                        stock_productos.update({key: stock_productos.get(key) - valor})
+                        print(stock_productos)
+                        precio_total += precio_productos.get(key)
+                        estado= "completo"
+                    else:
+                        stock_productos.update({key: 0})
+                        estado= "incompleto"
 
+        pedidos_procesados.append(pedido.update({"estado": estado}))
+    
+    
+        print(pedidos_procesados)
+
+    return pedidos_procesados
 
 if __name__ == "__main__":
     pedidos=[
@@ -26,10 +41,10 @@ if __name__ == "__main__":
         },{
             "id": 1,
             "cliente": "Juan",
-            "productos": {"Manzanas": 2, "Pan": 4, "Factura": 6}
+            "productos": {"Manzana": 2, "Pan": 4, "Factura": 6}
         }
     ]
             
-    print(type(stock_productos.get("Manzana")))
-    pedidos_procesados(pedidos)
+    #print(type(stock_productos.get("Manzana")))
+    print(procesar_pedidos(pedidos))
     
