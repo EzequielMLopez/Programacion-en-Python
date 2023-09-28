29193b9eca18322a82603ff65e9e6ef6d3b0e2d6
Ezequiel López <EzeMaxLop@hotmail.com>
2023-09-27 22:27:54 -0300

    Terminado Ejercicio

--- Curso_Alejandro/Ejercicio2.py
+++ Curso_Alejandro/Ejercicio2.py
@@ -2,24 +2,30 @@ stock_productos= {"Manzana": 10, "Leche": 5, "Pan": 3, "Factura": 0}
 precio_productos= {"Manzana": 3.5, "Leche": 5.5, "Pan": 3.5, "Factura": 2.75}
 
 def procesar_pedidos(pedidos):
-    precio_total= 0
-    estado= None
     pedidos_procesados= []
 
     for pedido in pedidos:
         precio_total= 0
+        estado= None
         for value in pedido.values():
             if type(value) == dict: 
                 for key, valor in value.items():
                     if valor < stock_productos.get(key):
                         stock_productos.update({key: stock_productos.get(key) - valor})
-                        precio_total += precio_productos.get(key)
+                        precio_total += (precio_productos.get(key) * valor)
                         estado= "completo"
                     else:
-                        stock_productos.update({key: 0})
-                        precio_total = precio_total + (precio_productos.get(key) * valor)
-                        estado= "incompleto"
+                        if stock_productos.get(key) > 0:
+                            print(precio_total)
+                            print(precio_productos.get(key))
+                            print(valor)
+                            precio_total += (precio_productos.get(key) * valor)
+                            #print(valor)
+                            #print(precio_total)
+                            stock_productos.update({key: 0})
+                            estado= "incompleto"
                         
+
         pedido.update({"estado": estado})
         pedido.update({"precio_total": precio_total})
         pedidos_procesados.append(pedido)
@@ -42,7 +48,7 @@ if __name__ == "__main__":
         },{
             "id": 1,
             "cliente": "Juan",
-            "productos": {"Manzana": 2, "Pan": 4, "Factura": 6}
+            "productos": {"Manzana": 2, "Pan": 3, "Factura": 6}
         }
     ]
             
