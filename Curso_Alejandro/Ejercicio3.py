d71a5c7b9f377bb0ea9a43777ba15f3621302da6
Ezequiel López <EzeMaxLop@hotmail.com>
2023-10-01 13:53:26 -0300

    Ejercicio3_tengo_dudas

---
+++ Curso_Alejandro/Ejercicio3.py
@@ -0,0 +1,15 @@
+def min_to_may(palabra):
+    nueva_palabra= ""
+    
+    for letra in palabra:
+        if letra in "abcdefghijklmnopqrstuvwxyz":
+            nueva_palabra+= letra.upper()
+        else:
+            nueva_palabra+= letra
+            continue
+    
+    return nueva_palabra
+
+
+palabrita= "Estocolmo es lo mEJor"
+print(min_to_may(palabrita))
