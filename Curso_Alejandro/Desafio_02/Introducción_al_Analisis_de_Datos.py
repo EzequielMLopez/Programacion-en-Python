d019b0c464cda13e1f0ba2db0e8c9cfb051d2595
Ezequiel López <EzeMaxLop@hotmail.com>
2023-10-08 20:55:15 -0300

    Se reaglizo hasta la segunda pregunta de la primera tanda de preguntas

--- Curso_Alejandro/Desafio_02/Introducción_al_Analisis_de_Datos.py
+++ Curso_Alejandro/Desafio_02/Introducción_al_Analisis_de_Datos.py
@@ -1,16 +1,29 @@
 import csv
 
 def art_cod_7462(registro):
-    with open(registro) as registre:
-        print(registre.read())
-        #next(registre, None)
-        #for line in registre:
-        #    line= line.rstrip()
-        #    lista= line.split(",")
-        #    nombre= lista[0]
-        #    print(nombre)
+    with open(registro, "r") as registre:
+        next(registre, None)
 
+        for line in registre:
+            line= line.rstrip()
+            lista= line.split(",")
+            if lista[1] == "7462":
+                print(f"El precio del articulo con código 7462 es: {lista[2]}")        
 
+def art_herramientas(registro):
+    with open(registro, "r") as registre:
+        next(registre, None)
+        herramientas= []
+
+        for line in registre:
+            line= line.rstrip()
+            lista= line.split(",")
+
+            if lista[4] == "Herramientas":
+                herramientas.append(lista[0])
+
+    print(f"En la categoría herramientas se cuenta con {len(herramientas)}")    
 
 registro= r"C:\Users\tetit\OneDrive\Escritorio\Programacion-en-Python\Curso_Alejandro\Desafio_02\registros_ferreteria.csv"
 art_cod_7462(registro)
+art_herramientas(registro)
