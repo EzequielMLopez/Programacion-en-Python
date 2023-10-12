e3f1930371ade1c9e404e7a94ce3c05d0f21e361
Ezequiel Maximiliano López <EzeMaxLop@hotmail.com>
2023-10-12 17:42:35 -0300

    Se agrego la variable registro_linux y se progreso hasta la pregunta 9

--- Curso_Alejandro/Desafio_02/Introducción_al_Analisis_de_Datos.py
+++ Curso_Alejandro/Desafio_02/Introducción_al_Analisis_de_Datos.py
@@ -71,10 +71,58 @@ def prov_caro(registro):
             
     print(f"El proveedor del artículo más caro es {proovedor} con un precio de {caro}")
 
-registro= r"C:\Users\tetit\OneDrive\Escritorio\Programacion-en-Python\Curso_Alejandro\Desafio_02\registros_ferreteria.csv"
+def peso_art_cod(registro):
+    with open(registro, "r") as registre:
+        next(registre, None)
+    
+        for line in registre:
+            line= line.rstrip()
+            lista= line.split(",")
+
+            if float(lista[1] == "5273"):
+                print(f"El peso del articulo {lista[0]} con código {lista[1]} es de {lista[6]}kg")
+
+def cat_art_vend(registro):
+    mas_vendido= 0
+    categoria= ""
+    with open(registro, "r") as registre:
+        next(registre, None)
+    
+        for line in registre:
+            line= line.rstrip()
+            lista= line.split(",")
+
+            if int(lista[9]) > mas_vendido:
+                mas_vendido= lista[9]
+                categoria= lista[4]
+                
+    print(f"La categoría del artículo más vendido, hablando de un total de {mas_vendido} por mes es {categoria}")
+
+def nom_art_bajo(registro):
+    mas_bajo= 9999999999
+    nombre_articulo= ""
+    with open(registro, "r") as registre:
+        next(registre, None)
+    
+        for line in registre:
+            line= line.rstrip()
+            lista= line.split(",")
+
+            if int(lista[2]) < mas_bajo:
+                nombre_articulo= lista[0]
+    
+    print(f"Se encontro el producto {nombre_articulo} con el precio más bajo")
+
+        
+registro_window= r"C:\Users\tetit\OneDrive\Escritorio\Programacion-en-Python\Curso_Alejandro\Desafio_02\registros_ferreteria.csv"
+registro_linux= r"/home/eml/Escritorio/Programacion-en-Python/Curso_Alejandro/Desafio_02/registros_ferreteria.csv"
 #art_cod_7462(registro)
 #art_herramientas(registro)
 #stock_martillo(registro)
 #mat_art_2930(registro)
 #desc_art(registro)
-prov_caro(registro)
+#prov_caro(registro_linux)
+#peso_art_cod(registro_linux)
+#cat_art_vend(registro_linux) # EN EL .CSV FALTAN LAS VENTAS MENSUALES
+#nom_art_bajo(registro_linux)
+
